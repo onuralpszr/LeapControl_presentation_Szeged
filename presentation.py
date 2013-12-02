28c6522e5e6c064bfdc6f197db720f6f719c83e1
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2013-12-02 12:16:37 +0200

    Presentation.py updated

---
+++ presentation.py
@@ -0,0 +1,182 @@
+
+
+import os
+import time
+import Leap, sys
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+from Xlib import X, display, ext, XK
+
+
+class Listener_desktop(Leap.Listener):
+
+    debug=0
+    screenx=0
+    screeny=0
+    scalex=1.0
+    scaley=1.0
+    offsety=20.
+    claw=0
+    switcher=0
+    rotationlock=0L
+    nframes=10 
+    frames=list()
+    prevframes=list()
+    rootw=0
+
+    def on_init(self, controller):
+        import subprocess, re
+        from time import sleep, clock
+
+        displ=display.Display()
+        s=displ.screen()
+        Listener_desktop.screenx=s.width_in_pixels
+        Listener_desktop.screeny=s.height_in_pixels
+        Listener_desktop.screenxmm=s.width_in_mms
+        Listener_desktop.screenymm=s.height_in_mms
+        Listener_desktop.scalex=s.width_in_pixels/300
+        Listener_desktop.scaley=s.height_in_pixels/300
+        Listener_desktop.rootw=s.root
+        Listener_desktop.display=displ
+
+        print "Screen size is:",Listener_desktop.screenx,Listener_desktop.screeny
+
+        if (Listener_desktop.debug == 1):
+            f=open('output.txt','w')
+            for ind in range(5): f.write(str(ind)+'\n')
+            f.close()
+        print 'Initializing Leap Motion...'
+        time0=clock()
+        while (not controller.frame(2*Listener_desktop.nframes).is_valid):
+            if clock()-time0 > 5:
+                print "Timeout waiting for valid frames from the Leap Motion."
+                print "Something's not right. Make sure the Leap Motion is connected"
+                print 'and the leapd daemon is running'
+                sys.exit(1)
+        for iframe in range(Listener_desktop.nframes):
+            Listener_desktop.frames.append(controller.frame(iframe))
+            Listener_desktop.prevframes.append(controller.frame(iframe+Listener_desktop.nframes))
+        print "Frame buffer sizes:",len(Listener_desktop.frames),len(Listener_desktop.prevframes)
+        print "Initialized"
+
+    def on_connect(self, controller):
+        print "Connected"
+
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+    def on_disconnect(self, controller):
+
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+
+        def flush_buffer():
+            Listener_desktop.prevframes[0:Listener_desktop.nframes]=Listener_desktop.frames[0:Listener_desktop.nframes]
+
+        import commands
+        from subprocess import call
+        from time import sleep
+
+        debug = Listener_desktop.debug
+        leftkey=Listener_desktop.display.keysym_to_keycode(XK.XK_Right)
+        rightkey=Listener_desktop.display.keysym_to_keycode(XK.XK_Left)
+        currentframe=controller.frame()
+
+        nframes2=Listener_desktop.nframes-10 
+        nframes=Listener_desktop.nframes
+
+        Listener_desktop.prevframes.pop(0)
+        Listener_desktop.prevframes.append(Listener_desktop.frames[0])
+        Listener_desktop.frames.pop(0)
+        Listener_desktop.frames.append(currentframe)
+
+        numfingers=len(currentframe.hands[0].fingers)
+
+        frame = controller.frame()
+        if not frame.hands.is_empty:
+            hand = frame.hands[0]
+            fingers = hand.fingers
+            if not fingers.is_empty:
+
+                avg_pos = Leap.Vector()
+                for finger in fingers:
+                    avg_pos += finger.tip_position
+                avg_pos /= len(fingers)
+
+            normal = hand.palm_normal
+            direction = hand.direction
+
+            for gesture in frame.gestures():
+                if gesture.type == Leap.Gesture.TYPE_CIRCLE:
+                    circle = CircleGesture(gesture)
+
+                    if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/4:
+                        clockwiseness = "clockwise"
+                        print "Next Slide"
+                        ext.xtest.fake_input(Listener_desktop.display, X.ButtonPress, 5)
+                        ext.xtest.fake_input(Listener_desktop.display, X.ButtonRelease, 5)
+                        Listener_desktop.display.flush()
+                        Listener_desktop.display.flush()
+                        flush_buffer()
+                        Listener_desktop.rotationlock = currentframe.timestamp                        
+                        time.sleep(1)
+                        #os.system("ssh pi@192.168.2.25 'cd /home/pi/Desktop/leap_booth/ && python pi_photobooth.py' ")
+                        #sys.exit("message")
+                        
+                    else:
+                        clockwiseness = "counterclockwise"
+                        print "Previous Slide"
+                        ext.xtest.fake_input(Listener_desktop.display, X.ButtonPress, 4)
+                        ext.xtest.fake_input(Listener_desktop.display, X.ButtonRelease, 4)
+                        Listener_desktop.display.flush()
+                        flush_buffer()
+                        Listener_desktop.rotationlock = currentframe.timestamp                        
+                        time.sleep(1)
+                        
+
+                if gesture.type == Leap.Gesture.TYPE_SWIPE & gesture.id > 0 & gesture.type != Leap.Gesture.TYPE_CIRCLE:
+                    swipe = SwipeGesture(gesture)
+                    print "Swipe id: %d, state: %s, position: %s, direction: %s, speed: %f" % (
+                            gesture.id, self.state_string(gesture.state),
+                            swipe.position, swipe.direction, swipe.speed)
+                    print ("Left to Right Swipe Gesture")
+                    time.sleep(2)
+                elif gesture.type == Leap.Gesture.TYPE_SWIPE & gesture.id < 0 & gesture.type != Leap.Gesture.TYPE_CIRCLE:
+                    print("Right to Left Swipe Gesture")
+                    time.sleep(2)
+
+    def state_string(self, state):
+        if state == Leap.Gesture.STATE_START:
+            return "STATE_START"
+
+        if state == Leap.Gesture.STATE_UPDATE:
+            return "STATE_UPDATE"
+
+        if state == Leap.Gesture.STATE_STOP:
+            return "STATE_STOP"
+
+        if state == Leap.Gesture.STATE_INVALID:
+            return "STATE_INVALID"
+
+
+
+
+def main():
+
+    listener = Listener_desktop()
+    controller = Leap.Controller()
+
+    controller.add_listener(listener)
+
+    print "Press Enter to quit..."
+    sys.stdin.readline()
+
+    controller.remove_listener(listener)
+
+if __name__ == "__main__":
+    main()
